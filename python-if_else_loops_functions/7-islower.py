1f5c1cf9340c5f6f1bb73a342e06ae1e5468e660
EugeneKhalilzade <elgynkhalilzade@gmail.com>
2025-11-25 20:08:06 +0000

    task 8.1

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 def islower(c):
-    if not isinstance(c, str) or len(c) != 1:
-        raise TypeError("c must be a single character string")
+    return ord('a') <= ord(c) <= ord('z')
 
-    return c.islower()
 
 islower = __import__('7-islower').islower
+

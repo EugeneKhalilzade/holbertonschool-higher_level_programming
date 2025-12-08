f05320982b8a826e67913f664723e84c58452c7e
EugeneKhalilzade <elgynkhalilzade@gmail.com>
2025-12-08 13:43:12 +0000

    task 1

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+        newtrix = []
+    for rows in matrix:
+        newrows = []
+        for i in rows:
+            newrows.append(i*i)
+        newtrix.append(newrows)
+    return newtrix

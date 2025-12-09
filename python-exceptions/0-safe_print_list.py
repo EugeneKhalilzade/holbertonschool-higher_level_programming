6246aa4d07bed6ada54793ebf949c3352c4c2668
EugeneKhalilzade <elgynkhalilzade@gmail.com>
2025-12-09 14:22:04 +0000

    task 1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    newlist = len(enumerate(my_list))
-    return newlist
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except Exception:
+        pass
+    print()
+    return count
+

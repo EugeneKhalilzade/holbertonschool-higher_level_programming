d92183e204883755947d78976a8f18ca1d212b19
EugeneKhalilzade <elgynkhalilzade@gmail.com>
2025-12-12 11:47:22 +0000

    #advanced task 3

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    removed_keys = []
+    for key, val in a_dictionary.items():
+        if val == value:
+            removed_keys.append(key)
+    for i in removed_keys:
+        a_dictionary.pop(i)
+    return a_dictionary 

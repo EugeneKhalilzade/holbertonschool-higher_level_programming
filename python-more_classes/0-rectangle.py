af6a4609db5a40cffb0d9857b5e6372a072c39a2
EugeneKhalilzade <elgynkhalilzade@gmail.com>
2025-12-18 11:26:10 +0000

    task 1

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    newtrix = []
-    for rows in matrix:
-        newrows = []
-        for i in rows:
-            newrows.append(i*i)
-        newtrix.append(newrows)
-    return newtrix
+"""
+This module defines an empty Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    This class represents a rectangle.
+    """
+    pass

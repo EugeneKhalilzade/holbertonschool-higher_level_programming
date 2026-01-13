0fd26df546d4ea840ddbdd50d0f9948953c61334
EugeneKhalilzade <elgynkhalilzade@gmail.com>
2026-01-13 20:48:52 +0000

    #advanced task 1

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
--- Convert database, table, and column to UTF8
--- Using utf8mb4 and utf8mb4_unicode_ci
+-- Convert database, table, and column to UTF8 (utf8mb4)
+-- Holberton School SQL task
 
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
@@ -7,9 +7,5 @@ COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE hbtn_0c_0.first_table
-MODIFY name VARCHAR(256)
 COLLATE utf8mb4_unicode_ci;
 

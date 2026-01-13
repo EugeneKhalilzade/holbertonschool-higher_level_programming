097bca88058c87299deb92df5d9e088dfd4d67a9
EugeneKhalilzade <elgynkhalilzade@gmail.com>
2026-01-13 20:46:23 +0000

    #advanced task 1

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,15 +1,15 @@
--- Convert database to utf8mb4
+-- Convert database, table, and column to UTF8
+-- Using utf8mb4 and utf8mb4_unicode_ci
+
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Convert table to utf8mb4
 ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Ensure column uses utf8mb4
 ALTER TABLE hbtn_0c_0.first_table
 MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
+
